8b194082eb700a0e86442ee0bb6f932a8265d129
Munkhtur <Munkhtur1231@gmail.com>
2024-02-04 15:25:05 +0800

    css zassan

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,4 @@
-const arr = [23, 44, 12];
+import "../css/style.css";
+import { add, multiply } from "./view/searchView";
 
-let myfunction = (a) => {
-  console.log(`too: ${a}`);
-};
-
-const arr2 = [...arr, 44, 1223];
-
-myfunction(arr2[2]);
-
-let b = [1, 2, 3].map((n) => n + 1);
-console.log(b);
+console.log(add(25, 50));
---
+++ src/js/view/searchView.js
@@ -0,0 +1,5 @@
+const supeAdd = (a, b) => a + b + 1000;
+
+export const add = (a, b) => supeAdd(a, b);
+export let multiply = (a, b) => a * b;
+export const id = 25;
